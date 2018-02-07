11b0ea7f41553b498fab12d91fd519c14f884553
Michael Bestas <mkbestas@lineageos.org>
2018-02-08 01:26:29 +0200

    sagit: Inherit from msm8998-common
    Change-Id: Ia50659e10e0e254a90dbbfee61cb53c0259774ad

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-#           (C) 2017 The LineageOS Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,48 +17,8 @@
 
 set -e
 
-DEVICE=sagit
-VENDOR=xiaomi
+export DEVICE=sagit
+export DEVICE_COMMON=msm8998-common
+export VENDOR=xiaomi
 
-INITIAL_COPYRIGHT_YEAR=2017
-
-# Load extractutils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
-
-MK_ROOT="$MY_DIR"/../../..
-
-HELPER="$MK_ROOT"/vendor/mk/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
-    exit 1
-fi
-. "$HELPER"
-
-# Initialize the helper for common
-setup_vendor "$DEVICE" "$VENDOR" "$MK_ROOT"
-
-# Copyright headers and guards
-write_headers
-
-# The standard common blobs
-write_makefiles "$MY_DIR"/proprietary-files.txt
-
-# We are done!
-write_footers
-
-if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
-    # Reinitialize the helper for device
-    INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
-    setup_vendor "$DEVICE" "$VENDOR" "$MK_ROOT" "false" "$1"
-
-    # Copyright headers and guards
-    write_headers
-
-    # The standard device blobs
-    write_makefiles "$MY_DIR"/../$DEVICE/proprietary-files.txt
-
-    # We are done!
-    write_footers
-fi
-
+./../../$VENDOR/$DEVICE_COMMON/setup-makefiles.sh $@
